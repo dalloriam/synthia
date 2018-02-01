20a58ec78d7a00978015c23402b61f702ed90b51
Dalloriam <dalloriam@gmail.com>
2018-02-01 13:46:51 -0500

    Add more doc

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+Package synthia is a modular synthesizer toolkit for Go.
+It features a core audio engine, contained in this package, as well as some modules in the [modular](https://godoc.org/github.com/Dalloriam/synthia/modular) package.
+*/
+package synthia
