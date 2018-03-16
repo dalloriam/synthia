144727e806e300683ae04b992322504cd493b737
William Dussault <dalloriam@gmail.com>
2018-03-15 22:32:45 -0400

    Improve tone generation

--- modular/oscillator.go
+++ modular/oscillator.go
@@ -35,6 +35,20 @@ func NewOscillator(freq float64, shape WaveShape) *Oscillator {
 	}
 }
 
+func (o *Oscillator) getToneGenerator() func() float64 {
+	var wave func() float64
+
+	switch o.Shape {
+	case SQUARE:
+		wave = o.square
+
+	case SINE:
+		wave = o.sine
+	}
+
+	return wave
+}
+
 func (o *Oscillator) incrementPhase(freq float64) {
 	if o.phase > 2*math.Pi {
 		o.phase = o.phase - (2 * math.Pi)
@@ -42,53 +56,32 @@ func (o *Oscillator) incrementPhase(freq float64) {
 	o.phase += freq * math.Pi / sampleRate
 }
 
-func (o *Oscillator) sine(p []float64) {
-	nbOfSamples := len(p)
-
-	volBuf := make([]float64, len(p))
-	o.Volume.Stream(volBuf)
-
-	phaseBuf := make([]float64, len(p))
-	o.Frequency.Stream(phaseBuf)
-
-	for i := 0; i < nbOfSamples; i++ {
-		volFactor := volBuf[i] / math.MaxFloat64
-		o.incrementPhase(phaseBuf[i])
-		sin := math.Sin(o.phase)
-
-		p[i] = sin * volFactor * math.MaxUint16 / 2
-	}
-
+func (o *Oscillator) sine() float64 {
+	return math.Sin(o.phase) * math.MaxUint16 / 2
 }
 
-func (o *Oscillator) square(p []float64) {
-	nbOfSamples := len(p)
-
-	var wv float64
-
-	volBuf := make([]float64, len(p))
-	o.Volume.Stream(volBuf)
-
-	phaseBuf := make([]float64, len(p))
-	o.Frequency.Stream(phaseBuf)
-
-	for i := 0; i < nbOfSamples; i++ {
-		o.incrementPhase(phaseBuf[i])
-		if math.Sin(o.phase) > 0 {
-			wv = math.MaxUint16 / 2
-		} else {
-			wv = -math.MaxUint16 / 2
-		}
-		p[i] = wv * (volBuf[i] / math.MaxFloat64)
+func (o *Oscillator) square() float64 {
+	if math.Sin(o.phase) > 0 {
+		return math.MaxUint16 / 2
+	} else {
+		return -math.MaxUint16 / 2
 	}
 }
 
 // Stream writes the current phase to the buffer.
 func (o *Oscillator) Stream(p []float64) {
-	switch o.Shape {
-	case SINE:
-		o.sine(p)
-	case SQUARE:
-		o.square(p)
+	toneGenerator := o.getToneGenerator()
+
+	nbOfSamples := len(p)
+
+	volBuf := make([]float64, len(p))
+	o.Volume.Stream(volBuf)
+
+	phaseBuf := make([]float64, len(p))
+	o.Frequency.Stream(phaseBuf)
+
+	for i := 0; i < nbOfSamples; i++ {
+		o.incrementPhase(phaseBuf[i])
+		p[i] = toneGenerator() * (volBuf[i] / math.MaxFloat64)
 	}
 }
