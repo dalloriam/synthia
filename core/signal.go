a5de993f65eefecb72a149ac90d3564e9138aecc
Dalloriam <dalloriam@gmail.com>
2018-08-05 23:51:06 -0400

    Fix typo

--- core/signal.go
+++ core/signal.go
@@ -5,7 +5,7 @@ type Signal interface {
 	Stream() float64
 }
 
-// A SteroSignal represents a buffered stereo signal source. (Ex: an audio mixer)
+// A StereoSignal represents a buffered stereo signal source. (Ex: an audio mixer)
 type StereoSignal interface {
 	Stream(l, r []float64)
 }
